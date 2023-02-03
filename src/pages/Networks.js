ca7b112ce446b226853cb7ade5600490e22c7100
lionel <lionelbos@gmail.com>
2023-02-04 00:28:39 +0100

    css 04022023

--- src/pages/Networks.js
+++ src/pages/Networks.js
@@ -6,22 +6,20 @@ const Networks = () => {
         <div className="networks">
             <Navigation />
             <div className="networksContent">
-                <div className="content">
-                     <ul >
-                        <li>
-                            <a href="https://linkedin.com/in/lionel-b-837660201" target="_blank" rel="noreferrer"><i className="fab fa-linkedin" /></a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/JustVNRR" target="_blank" rel="noreferrer"><i className="fab fa-github" /></a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/lionel_bos" target="_blank" rel="noreferrer"><i className="fab fa-twitter" /></a>
-                        </li>
-                        <li>
-                            <a href="https://codepen.io/JustVNRR/pens/public" target="_blank" rel="noreferrer"><i className="fab fa-codepen" /></a>
-                        </li>
-                    </ul>
-                </div>
+                <ul >
+                    <li>
+                        <a href="https://linkedin.com/in/lionel-b-837660201" target="_blank" rel="noreferrer"><i className="fab fa-linkedin" /></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/JustVNRR" target="_blank" rel="noreferrer"><i className="fab fa-github" /></a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/lionel_bos" target="_blank" rel="noreferrer"><i className="fab fa-twitter" /></a>
+                    </li>
+                    <li>
+                        <a href="https://codepen.io/JustVNRR/pens/public" target="_blank" rel="noreferrer"><i className="fab fa-codepen" /></a>
+                    </li>
+                </ul>
             </div>
         </div>
     );
