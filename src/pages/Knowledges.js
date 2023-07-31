0a72ef5ce8f872d8b8bcfd6e0adaf25db38e6077
lionel <lionelbos@gmail.com>
2023-07-31 13:40:12 +0200

    scrollbar

--- src/pages/Knowledges.js
+++ src/pages/Knowledges.js
@@ -4,20 +4,23 @@ import Experience from '../components/knowledges/Experience';
 import Education from '../components/knowledges/Education';
 import Languages from '../components/knowledges/Languages';
 import Navigation from '../components/Navigation';
+import Scrollbar from '../components/Scrollbar';
 import OtherSkills from '../components/knowledges/OtherSkills';
 
 const Knowledges = () => {
     return (
         <div className="knowledges">
             <Navigation />
-            <div className="knowledgesContent">
-                <div className="knowledgesWrapperContent">
-                    <Languages />
-                    <Experience />
-                    <OtherSkills />
-                    <Education />
-                </div>
-            </div>
+            {/* <div className="knowledgesContent"> */}
+                <Scrollbar>
+                    <div className="knowledgesWrapperContent">
+                        <Languages />
+                        <Experience />
+                        <OtherSkills />
+                        <Education />
+                    </div>
+                </Scrollbar>
+            {/* </div> */}
         </div>
     );
 };
